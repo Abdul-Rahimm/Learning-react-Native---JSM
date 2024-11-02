2243b600056e21a8ddf04fef983eaf9f3dd493ed
Abdul Rahim <k213951@nu.edu.pk>
2024-11-02 10:03:49 +0500

    created DB on appwrite and 2 collections. users and videos.

---
+++ aora/lib/appwrite.js
@@ -0,0 +1,8 @@
+export const appwriteConfig = {
+  endpoint: "https://cloud.appwrite.io/v1",
+  platform: "com.jsm.aora",
+  projectId: "6725ae2a0025c8218732",
+  databaseId: "6725b0020000f9762f32",
+  userCollectionId: "6725b063001ec6576265",
+  videoCollectionId: "6725b09d003c59c58b27",
+};
